c81e6f771aef754f8f9f9dd9c24bf6cf9e225206
Eva van Geenen <evavangeenen@gmail.com>
2020-08-21 11:38:42 +0200

    Added WebClientConfig with a json decoder, a pricing client as registrationId and a ReactorLoadBalancerExchangeFilterFunction... Say that again? A ReactorLoadBalancerExchangeFilterFunction... Practicing on saying it backwards...

---
+++ src/main/java/com/okta/developer/cartservice/WebClientConfig.java
@@ -0,0 +1,48 @@
+package com.okta.developer.cartservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Autowired
+    private ReactorLoadBalancerExchangeFilterFunction lbFunction;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Bean
+    public WebClient webClient(ClientRegistrationRepository clientRegistrations,
+                               OAuth2AuthorizedClientRepository oAuth2AuthorizedClients){
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer ->
+                        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper)))
+                .build();
+
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oAuth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, oAuth2AuthorizedClients);
+
+        oAuth2.setDefaultClientRegistrationId("pricing-client");
+
+        return WebClient.builder()
+                .apply(oAuth2.oauth2Configuration())
+                .exchangeStrategies(exchangeStrategies)
+                .filter(lbFunction)
+                .baseUrl("http://pricing/pricing/price")
+                .build();
+
+    }
+
+}
