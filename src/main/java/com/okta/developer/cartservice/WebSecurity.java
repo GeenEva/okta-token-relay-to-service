44397e19ef09383b5bfd2f60ae90d119d92af4dd
Eva van Geenen <evavangeenen@gmail.com>
2020-08-21 12:06:49 +0200

    Added Pricing Exception class, to return status 500 for when the cart cannot be priced due to an unexpected error.

--- src/main/java/com/okta/developer/cartservice/WebSecurity.java
+++ src/main/java/com/okta/developer/cartservice/WebSecurity.java
@@ -1,9 +1,18 @@
 package com.okta.developer.cartservice;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
+
+/*
+Spring Boot will auto-configure the application as an OAuth2 client because of the client.registration presence in the YAML.
+Add a src/main/java/com/okta/developer/cartservice/WebSecurity.java class to override the auto-configuration,
+and configure the application as an OAuth 2.0 resource server.
+ */
+
+@Configuration
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
